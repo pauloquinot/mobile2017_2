378f8c775584122058d9953efefafb6246dee135
Paulo Quinot <pauloquinot@hotmail.com>
2017-08-21 23:43:35 -0300

    Atualização

--- TarefaCSharp/TarefaCSharp/Program.cs
+++ TarefaCSharp/TarefaCSharp/Program.cs
@@ -37,6 +37,7 @@ namespace TarefaCSharp
                         Program.Altera();
                         break;
                     case "3":
+                        Program.Exclui();
                         break;
                     case "4":
                         foreach(var produto in listaProdutos)
@@ -71,15 +72,69 @@ namespace TarefaCSharp
         }
         public static void Altera()
         {
-            Produto novoProduto = new Produto();
+            Int32 codAlterar = 0;
+            int indAlterar = -1;
             Console.Clear();
             Console.WriteLine("************ Alterar ************\n\n");
             Console.Write("Digire o código do produto a alterar: ");
-            novoProduto.Codigo = Convert.ToInt32(Console.ReadLine());
-            Console.Write("\nNome: ");
-            novoProduto.Nome = Console.ReadLine();
-            listaProdutos.Add(novoProduto);
-        }
+            codAlterar = Int32.Parse(Console.ReadLine());
 
+            for (int i = 0; i < listaProdutos.Count; i++)
+            {
+                if(listaProdutos[i].Codigo == codAlterar)
+                {
+                    indAlterar = i;
+                }
+            }
+            if (indAlterar > 0)
+            {
+                Console.Write("\nNome atual: " + listaProdutos[indAlterar].Nome);
+                Console.Write("\n\nDigite o novo nome: ");
+                listaProdutos[indAlterar].Nome = Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("\n\nCódigo não encontrado!!! \n\nPressione uma tecla!");
+                Console.ReadKey();
+            }
+        }
+        public static void Exclui()
+        {
+            Int32 codExcluir = 0;
+            int indExcluir = -1;
+            string confExclusao = "";
+            Console.Clear();
+            Console.WriteLine("************ Excluir ************\n\n");
+            Console.Write("Digire o código do produto a excluir: ");
+            codExcluir = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < listaProdutos.Count; i++)
+            {
+                if (listaProdutos[i].Codigo == codExcluir)
+                {
+                    indExcluir = i;
+                }
+            }
+            if (indExcluir > 0)
+            {
+                Console.Write("\nProduto: " + listaProdutos[indExcluir].Codigo + " - " + listaProdutos[indExcluir].Nome + "\n\n");
+                Console.Write("\n\nConfirma Exclusão? (S/N): ");
+                confExclusao = Console.ReadLine();
+                if(confExclusao == "S" || confExclusao == "s")
+                {
+                    listaProdutos.RemoveAt(indExcluir);
+                }
+                else
+                {
+                    Console.WriteLine("\n\nExclusão não confirmada!! \n\nPressione qualquer tecla!!");
+                    Console.ReadKey();
+                }
+            }
+            else
+            {
+                Console.WriteLine("\n\nCódigo não encontrado!!! \n\nPressione uma tecla!");
+                Console.ReadKey();
+            }
+        }
     }
 }
